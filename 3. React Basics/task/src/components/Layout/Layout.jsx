6bddfd7746d2663cd5781c74b452e058248ed076
German Ivanov <germanivanov0719@gmail.com>
2022-11-04 13:28:48 +0300

    Reenable ESLint

--- 3. React Basics/task/src/components/Layout/Layout.jsx
+++ 3. React Basics/task/src/components/Layout/Layout.jsx
@@ -4,18 +4,18 @@ import BookCard from '../BookCard/BookCard';
 
 const styleParent = {
   display: 'flex',
-}
+};
 
-const genres = ['Художественная литература','Нехудожественная литература','Книги для детей','Переодические издания','Религия','Учебная литература', 'Комиксы'];
+const genres = ['Художественная литература', 'Нехудожественная литература', 'Книги для детей', 'Переодические издания', 'Религия', 'Учебная литература', 'Комиксы'];
 
 export default function Layout(props) {
-  console.log(props.books);
+  // console.log(props.books);
   const [genre, setGenre] = useState(0);
   return (
     <div style={styleParent}>
-      <div >
+      <div>
         <ul>
-          <li onClick={() => {setGenre(0)}}>Художественная литература</li>
+          <li onClick={() => { setGenre(0); }}>Художественная литература</li>
           <li onClick={() => {}}>Нехудожественная литература</li>
           <li onClick={() => {}}>Книги для детей</li>
           <li onClick={() => {}}>Переодические издания</li>
