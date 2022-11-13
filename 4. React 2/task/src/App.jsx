72fb9383ca862136bb7ac289d062f326bd2e9dd1
German Ivanov <germanivanov0719@gmail.com>
2022-11-13 14:48:30 +0300

    More useless changes

--- 4. React 2/task/src/App.jsx
+++ 4. React 2/task/src/App.jsx
@@ -1,15 +1,16 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import BookPage from "./pages/BookPage/BookPage";
-import Layout from "./components/Layout/Layout";
 import Navbar from "./components/Navbar/Navbar";
-import { Provider } from "react-redux";
-import {store} from './store';
-import HomePage from './pages/HomePage/HomePage'
+import { Provider, useDispatch } from "react-redux";
+import { store } from "./store";
+import HomePage from "./pages/HomePage/HomePage";
 import { data } from "./constants/constants";
-import {NotFoundPage} from './pages/NotFoundPage/NotFoundPage';
+import { NotFoundPage } from "./pages/NotFoundPage/NotFoundPage";
+import { loadCategoryIfNotExist } from "./store/category/loadCategoryIfNotExist";
 
 export default function App() {
+  const dispatch = useDispatch();
   const [chosenBook, setChosenBook] = useState(data[0]);
   const getAllGenres = () => {
     let allGenres = [];
@@ -21,19 +22,29 @@ export default function App() {
   const handleChosenBook = (book) => {
     setChosenBook(book);
   };
+  useEffect(() => {
+    dispatch(loadCategoryIfNotExist());
+  }, []);
   return (
-    <Provider store={store}>
+    // <Provider store={store}>
       <BrowserRouter>
-        <Layout books={data} genres={genres} handleChosenBook={handleChosenBook}>
-          <Routes>
-          <Route path="/">
-            <Route index element={<HomePage />} />
-            <Route path="properBook" element={<BookPage />} />
-            <Route path="*" element={<NotFoundPage />} />
-          </ Route>
-          </Routes>
-        </Layout>
+        <Navbar />
+        <Routes>
+          <Route
+            index
+            path="/"
+            element={
+              <HomePage
+                books={data}
+                genres={genres}
+                handleChosenBook={handleChosenBook}
+              />
+            }
+          />
+          <Route path="/book" element={<BookPage book={chosenBook} />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
       </BrowserRouter>
-    </Provider>
+    // </Provider>
   );
 }
