f3788e9b37761c7ebc02777aeecc55408bdfcde3
German Ivanov <germanivanov0719@gmail.com>
2022-11-13 20:37:30 +0300

    Home page works now!

--- 4. React 2/task/src/components/Layout/Layout.jsx
+++ 4. React 2/task/src/components/Layout/Layout.jsx
@@ -2,56 +2,55 @@ import "./Layout.css";
 import BookCard from "../BookCard/BookCard";
 import { useState } from "react";
 import { useSelector } from "react-redux";
-import { useEffect } from 'react';
+import { useEffect } from "react";
 
 const styleParent = {
   display: "flex",
   gap: "2em",
 };
 
-export default function Layout({handleChosenBook}) {
-  const categories = Object.values(useSelector((state) => state.category).entities);
-  const books = useSelector((state) => state.books);
-  const [currentCategoryId, setCurrentCategoryId] = useState(0);
-  console.log(books)
-  // console.log(books
-  //   .filter((book) => book.id in categories[currentCategoryId].books )
-  //   .map((book) => (
-  //     <BookCard
-  //       key={book.bookId}
-  //       book={book}
-  //       handleChosenBook={handleChosenBook}
-  //     />
-  // )));
+export default function Layout({ handleChosenBook }) {
+  const categories = Object.values(
+    useSelector((state) => state.category).entities
+  );
+  const books = useSelector((state) => state.book).entities;
+  const [currentCategoryId, setCurrentCategoryId] = useState(1);
+  console.log(books);
   return (
     <div>
-      {categories.length > 0 ? (
+      {categories.length > 0 && books !== undefined ? (
         <div style={styleParent} className="content-container">
           <ul className="card genre-list ">
             {categories.map((genre) => (
               <li
-                className={currentCategoryId === genre ? "bold" : ""}
-                key={categories.indexOf(genre)}
+                className={currentCategoryId === genre.id ? "bold" : ""}
+                key={genre.id}
                 onClick={() => {
-                  setCurrentCategoryId(genre);
+                  setCurrentCategoryId(genre.id);
                 }}>
                 {genre.name}
               </li>
             ))}
           </ul>
-          {/* <ul className="book-list">
+          <ul className="book-list">
             {books
-              .filter((book) => book.id in categories[currentCategoryId].books )
+              .filter((book) =>
+                categories
+                  .find((category) => category.id === currentCategoryId)
+                  .books.includes(book.id)
+              )
               .map((book) => (
                 <BookCard
-                  key={book.bookId}
+                  key={book.id}
                   book={book}
                   handleChosenBook={handleChosenBook}
                 />
               ))}
-          </ul> */}
+          </ul>
         </div>
-      ) : ''}
+      ) : (
+        ""
+      )}
     </div>
   );
 }
